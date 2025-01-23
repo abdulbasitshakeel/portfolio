ed776bdd4d182be0632511b096a3adb99bf1a52d
Abdul Basit Shakeel <basitshakeel2005@gmail.com>
2025-01-23 10:30:54 +0500

    workk done 3

--- index.php
+++ index.php
@@ -20,8 +20,7 @@
 <body id="page-top">
 <nav class="navbar navbar-b navbar-trans navbar-expand-md fixed-top rounded-navbar" id="mainNav">
   <div class="container d-flex justify-content-between align-items-center">
-    <span class="navbar-brand mb-0 h1"><a href="index.php" style="font-family: Pacifico;
-  font-weight: 400;font-size:38px;color:white!important;margin-left:20px;">ABS</a></span>
+    <span class="navbar-brand mb-0 h1"><a href="index.php" style="font-family: Pacifico; font-weight: 400; font-size:38px; color:white!important; margin-left:10px;">ABS</a></span>
     <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarDefault"
       aria-controls="navbarDefault" aria-expanded="false" aria-label="Toggle navigation">
       <span></span>
@@ -29,7 +28,7 @@
       <span></span>
     </button>
     <div class="navbar-collapse collapse justify-content-center" id="navbarDefault">
-      <ul class="navbar-nav">
+      <ul class="navbar-nav mx-auto">
         <li class="nav-item">
           <a class="nav-link js-scroll active" href="index.php">Home</a>
         </li>
@@ -37,16 +36,16 @@
           <a class="nav-link js-scroll" href="#about">About</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link js-scroll" href="#service">Services</a>
+          <a class="nav-link js-scroll" href="#services">Services</a>
         </li>
         <li class="nav-item">
           <a class="nav-link js-scroll" href="#work">Project</a>
         </li>
       </ul>
-    </div>
-    <div class="d-flex flex-row align-items-center">
-      <a class="btn btn-outline-light resume-btn ms-3" href="#">Resume</a>
-      <a class="btn btn-light hire-btn" href="#">Hire Me</a>
+      <div class="d-flex flex-row align-items-center ml-auto">
+        <a class="btn btn-outline-light resume-btn ms-3" href="#">Resume</a>
+        <a class="btn btn-light hire-btn" href="#">Hire Me</a>
+      </div>
     </div>
   </div>
 </nav>
@@ -78,7 +77,7 @@
           </div>
         </div>
     </div>
-  <section id="about" class="about-mf sect-pt4 route">
+  <section id="about" class="about-mf sect-pt4 route position-sticky">
     <div class="container">
       <div class="row">
         <div class="col-sm-12">
@@ -110,35 +109,45 @@
               <div class="col-md-12">
                 <div class="skill-mf">
                   <h5 class="title-left mb-5">Skills</h5>
-                  <span>HTML5</span> <span class="pull-right">100%</span>
+                  <div class="d-flex justify-content-between">
+                    <span>HTML5</span> <span class="pull-right">100%</span>
+                  </div>
                   <div class="progress">
                     <div class="progress-bar rounded" role="progressbar" style="width: 100%;" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
                   </div>
-                  <span>CSS3</span> <span class="pull-right">95%</span>
+                  <div class="d-flex justify-content-between">
+                    <span>CSS3</span> <span class="pull-right">95%</span>
+                  </div>
                   <div class="progress">
                     <div class="progress-bar rounded" role="progressbar" style="width: 95%;" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100"></div>
                   </div>
-                  <span>Javascript</span> <span class="pull-right">75%</span>
+                  <div class="d-flex justify-content-between">
+                    <span>Javascript</span> <span class="pull-right">75%</span>
+                  </div>
                   <div class="progress">
                     <div class="progress-bar rounded" role="progressbar" style="width: 75%;" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
                   </div>
-                  <span>Bootstrap</span> <span class="pull-right">90%</span>
+                  <div class="d-flex justify-content-between">
+                    <span>Bootstrap</span> <span class="pull-right">90%</span>
+                  </div>
                   <div class="progress">
                     <div class="progress-bar rounded" role="progressbar" style="width: 90%;" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
                   </div>
-                  <span>JQuery</span> <span class="pull-right">50%</span>
+                  <div class="d-flex justify-content-between">
+                    <span>JQuery</span> <span class="pull-right">50%</span>
+                  </div>
                   <div class="progress">
                     <div class="progress-bar rounded" role="progressbar" style="width: 50%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
                   </div>
-                  <span>MySQL</span> <span class="pull-right">80%</span>
+                  <div class="d-flex justify-content-between">
+                    <span>MySQL</span> <span class="pull-right">80%</span>
+                  </div>
                   <div class="progress">
                     <div class="progress-bar rounded" role="progressbar" style="width: 80%;" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
                   </div>
-                  <span>PHP</span> <span class="pull-right">70%</span>
-                  <div class="progress">
-                    <div class="progress-bar rounded" role="progressbar" style="width: 70%;" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
+                  <div class="d-flex justify-content-between">
+                    <span>Full Website Creation</span> <span class="pull-right">100%</span>
                   </div>
-                  <span>Full Website Creation</span> <span class="pull-right">100%</span>
                   <div class="progress">
                     <div class="progress-bar rounded" role="progressbar" style="width: 100%;" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
                   </div>
@@ -150,29 +159,84 @@
       </div>
     </div>
   </section>
-  <footer>
-    <div class="container">
+  <section id="services" class="about-mf route position-sticky">
+  <div class="container">
+    <div class="box-shadow-full">
+      <h5 class="title-left mb-4 pt-3">Services</h5>
       <div class="row">
-        <div class="col-sm-12">
-          <div class="copyright-box">
-            <p class="copyright">2025 All Rights by <a href="#page-top" class="text-decoration-none text-dark">Basit Shakeel </a> Reserved</p>
+        <div class="col-md-4">
+          <div class="card mt-5">
+            <div class="icon">
+              <i class="fa-solid fa-pen-nib"></i>
+            </div>
+            <h2>Web Design</h2>
+            <p>I am a skilled web designer committed to crafting unique, user friendly websites. My designs focus on both aesthetics and functionality, ensuring your website not only looks great but also performs well.</p>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mt-5">
+            <div class="icon">
+              <i class="fa-solid fa-code"></i>
+            </div>
+            <h2>Web Development</h2>
+            <p>I specialize in web development, creating scalable websites that meet your needs. My focus is on building high-performance sites using the latest technologies to ensure security and reliability.</p>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mt-5">
+            <div class="icon">
+            <i class="fa-solid fa-bag-shopping"></i>
+            </div>
+            <h2>E-Commerce Platform</h2>
+            <p>I offer E-commerce services to help you build a powerful online store. From user friendly design to secure payment systems, I ensure your E-commerce website is optimized for sales, performance, and growth</p>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-4">
+          <div class="card mt-5">
+            <div class="icon">
+              <i class="fab fa-apple" alt="Apple Icon"></i>
+            </div>
+            <h2>App Design</h2>
+            <p>I specialize in app development, creating high-performance apps with the latest technologies for security and seamless user experience</p>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card mt-5">
+            <div class="icon">
+              <i class="fas fa-globe-europe" alt="Globe Icon"></i>
+            </div>
+            <h2>Marketing</h2>
+            <p>I specialize in marketing, helping businesses grow through targeted strategies and effective campaigns that drive engagement and increase sales.</p>
           </div>
         </div>
       </div>
     </div>
-  </footer>
-  <div id="preloader"></div>
-  <script src="assets/js/jquery.min.js"></script>
-  <script src="assets/js/popper.min.js"></script>
-  <script src="assets/js/bootstrap.min.js"></script>
-  <script src="assets/js/easing.min.js"></script>
-  <script src="assets/js/jquery.waypoints.min.js"></script>
-  <script src="assets/js/jquery.counterup.js"></script>
-  <script src="assets/js/owl.carousel.min.js"></script>
-  <script src="assets/js/lightbox.min.js"></script>
-  <script src="assets/js/typed.min.js"></script>
-  <script src="assets/js/main.js"></script>
-  <script src="assets/js/particles.js"></script>
-
+  </div>
+</section>
+<footer  class="pb-3">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="copyright-box">
+          <p class="copyright">2025 All Rights by <a href="#page-top" class="text-decoration-none text-dark">Basit Shakeel </a> Reserved</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
+<div id="preloader"></div>
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/popper.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/easing.min.js"></script>
+<script src="assets/js/jquery.waypoints.min.js"></script>
+<script src="assets/js/jquery.counterup.js"></script>
+<script src="assets/js/owl.carousel.min.js"></script>
+<script src="assets/js/lightbox.min.js"></script>
+<script src="assets/js/typed.min.js"></script>
+<script src="assets/js/main.js"></script>
+<script src="assets/js/particles.js"></script>
 </body>
 </html>
